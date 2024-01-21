3690c4cfc628cdb217c91afef502071d5e99453f
Francesco Ciociola <francesco.ciociola01@gmail.com>
2024-01-21 11:16:33 +0100

    Fix messaggio iscrizione e invio messaggio a tutti
    Ho fixato il messaggio del "Non sei iscritto al Babbo Natale Segreto" perché lo inviava anche a chi era effettivamente iscritto.
    Ho modificato il file inviodestinatario.py con l'invio del messaggio a chi è iscritto con chi deve fare il regalo.

--- inviodestinatario.py
+++ inviodestinatario.py
@@ -6,9 +6,12 @@ mycursor.execute(sql)
 myresult = mycursor.fetchall()
 
 for x in myresult:
-    try:
-        time.sleep(2)
-        telegrambot.sendMessage(x[0], "Ciao " + x[1] + ", questo è un messaggio prova! 👌👌")
-    except:
-        print(x[0])
+
+    time.sleep(2)
+    sql = "SELECT nome FROM partecipanti WHERE chat_id=" + str(x[2])
+    mycursor = mydb.cursor()
+    mycursor.execute(sql)
+    myresult2 = mycursor.fetchone()
+    telegrambot.sendMessage(x[0], "Allora " + x[1] + " devi fare il regalo a " + myresult2[0])
+
 mycursor.close()
